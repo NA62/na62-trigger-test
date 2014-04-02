d00e32024f8b29ec52849443609e4f2253b464bd
Jonas Kunze <kunze.jonas@gmail.com>
2014-04-02 14:40:33 +0200

    First basic implementation

---
+++ src/main.cpp
@@ -0,0 +1,20 @@
+//============================================================================
+// Name        : NA62 online trigger algorithm test
+// Author      : Jonas Kunze (kunze.jonas@gmail.com)
+//============================================================================
+
+#include <eventBuilding/Event.h>
+#include <l1/L1TriggerProcessor.h>
+#include <options/Options.h>
+
+using namespace na62;
+int main(int argc, char* argv[]) {
+	/*
+	 * Static Class initializations
+	 */
+	Options::Initialize(argc, argv);
+
+	Event* e = new Event(0);
+	L1TriggerProcessor t;
+	return 0;
+}
