5ad93403de0c44bae901288605ad040d90abdbb2
Jonas Kunze <kunze.jonas@gmail.com>
2014-07-08 16:45:10 +0200

    Added own Options file

--- src/main.cpp
+++ src/main.cpp
@@ -5,14 +5,15 @@
 
 #include <eventBuilding/Event.h>
 #include <l1/L1TriggerProcessor.h>
-#include "../options/MyOptions.h"
+
+#include "options/MyOptions.h"
 
 using namespace na62;
 int main(int argc, char* argv[]) {
 	/*
 	 * Static Class initializations
 	 */
-	Options::Initialize(argc, argv);
+	MyOptions::Load(argc, argv);
 
 	Event* e = new Event(0);
 	L1TriggerProcessor t;
---
+++ src/options/MyOptions.h
@@ -0,0 +1,46 @@
+/*
+ * MyOptions.h
+ *
+ *  Created on: Apr 11, 2014
+ \*      Author: Jonas Kunze (kunze.jonas@gmail.com)
+ */
+
+#pragma once
+#ifndef MYOPTIONS_H_
+#define MYOPTIONS_H_
+
+#include <options/Options.h>
+#include <string>
+#include <boost/thread.hpp>
+
+/*
+ * Available options
+ */
+
+/*
+ * Performance
+ */
+#define OPTION_ZMQ_IO_THREADS (char*)"zmqIoThreads"
+
+namespace na62 {
+class MyOptions: public Options {
+public:
+	MyOptions();
+	virtual ~MyOptions();
+
+	static void Load(int argc, char* argv[]) {
+		desc.add_options()
+
+		(OPTION_CONFIG_FILE,
+				po::value<std::string>()->default_value(
+						"na62-trigger-test.conf"),
+				"Config file for the options shown here")
+
+				;
+
+		Options::Initialize(argc, argv, desc);
+	}
+};
+} /* namespace na62 */
+
+#endif /* MYOPTIONS_H_ */
